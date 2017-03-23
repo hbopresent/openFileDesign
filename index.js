ad6e6ea1a6ee96fd3b2272df7c8c01cddb4ff5dd
hbopresent <hbopresent@gmail.com>
2017-03-23 11:58:12 +0800

    update

--- index.js
+++ index.js
@@ -25,22 +25,75 @@ myFolder = (function(global) {
   var uploadFiles = function() {
     var focusedFileArray = document.getElementsByClassName("focusedFile");
     for(var i = 0 ; i < focusedFileArray.length ; i++) {
-      createProgressBar(focusedFileArray[i]);
+      // the file has been already uploading
+      if(focusedFileArray[i].lastElementChild.nodeName == "div") {
+        continue;
+      }
+      // the file is going to be uploaded
+      if(focusedFileArray[i].lastElementChild.nodeName == "svg") {
+        // return the progressBar and appended in the file item
+        var focusedFileProgressBar = createProgressBar(focusedFileArray[i], i);
+        focusedFileArray[i].appendChild(focusedFileProgressBar);
+        // choose uploading animation type randomly
+        var progressBarAnimationType = Math.floor(Math.random() * 2) + 1;
+        if(progressBarAnimationType == 1) {
+          focusedFileProgressBar.firstElementChild.style.animation = "uploadAnimation1 0.5s linear forwards, uploadAnimation2 0.5s 0.5s linear forwards";
+        }
+        if(progressBarAnimationType == 2) {
+          focusedFileProgressBar.firstElementChild.style.animation = "uploadAnimation1 0.5s linear forwards, uploadAnimation2 0.5s 1.5s linear forwards";
+        }
+
+        blurFileItem(focusedFileArray[i], i);
+      }
     }
   };
 
-  var createProgressBar = function(focusedItem) {
+  var createProgressBar = function(focusedItem, idNum) {
+    // create new elements
+    var progressBar = document.createElement("div");
+    var progressState = document.createElement("div");
+    var successIconSvg = document.createElement("svg");
+    var path = document.createElement("path");
 
+    // set attributes
+    progressBar.classList.add("progressBar");
+    progressBar.setAttribute("id", "progressBar" + idNum);
+    progressState.classList.add("progressState");
+    successIconSvg.classList.add("successIconSvg");
+    path.classList.add("successIconSvgPath");
+    path.setAttribute("d", "M1 10 L5 14 L14 3");
+
+    // append children
+    successIconSvg.appendChild(path);
+    progressBar.appendChild(progressState);
+    progressBar.appendChild(successIconSvg);
+
+    return progressBar;
   }
 
   // set focus state when the file clicked
   var focusFileItem = function(fileObj) {
+    // js will detect the class "focusedFile" to upload files
     fileObj.classList.add("focusedFile");
     fileObj.firstElementChild.style.color = "#000";
     var fileObjSvgChild = fileObj.firstElementChild.nextElementSibling;
     fileObjSvgChild.firstElementChild.classList.add("foucsFileSvg");
     fileObjSvgChild.lastElementChild.classList.add("foucsFileCornerSvg");
   };
+
+  // blur focus state after files have been upload
+  var blurFileItem = function(fileObj, idNum) {
+    // remove focus property
+    // remove progressbar
+    setTimeout(function() {
+      fileObj.classList.remove("focusedFile");
+      fileObj.removeChild(fileObj.lastElementChild);
+      fileObj.firstElementChild.style.color = "#FFF";
+      var fileObjSvgChild = fileObj.firstElementChild.nextElementSibling;
+      fileObjSvgChild.firstElementChild.classList.remove("foucsFileSvg");
+      fileObjSvgChild.lastElementChild.classList.remove("foucsFileCornerSvg");
+    }, 3500);
+  }
 
   var openFileAnimation = function() {
     folderCover.style.animation = "openFolderCover 0.3s ease forwards";
